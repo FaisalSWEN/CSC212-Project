fd445bfe1e4838666f9a22ba26f31d8df2b230f9
FaisalSWEN <FaisalSWENBusiness@outlook.com>
2023-10-17 21:01:15 +0300

    a

--- Phonebook.java
+++ Phonebook.java
@@ -1,27 +1,9 @@
-/************* Project ***************
-  CLASS: Phonebook.java
-  CSC212 Data structures - Project phase I
-  Fall 2023
-
-  EDIT DATE:
-  10-17-2023
-
-  TEAM:
-
-  Faisal AlBader - 443102460
-  Bader Alshehri - 443100744
-  Turki Alhussan - 443101793
-
-  AUTHORS:
-  Faisal AlBader , (ID: 443102460)
-***********************************/
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.text.ParseException;
-
 /*------------------------------------------------------------------------------------
   Criterias enumerations: specifically listed Criterias
   ------------------------------------------------------------------------------------*/
